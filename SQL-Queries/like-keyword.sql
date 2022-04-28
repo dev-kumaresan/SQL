236b5441c10b78d03a644a0197724eca6d6b99af
dev-kumaresan <dev.kumaresanl@gmail.com>
2022-04-28 20:54:59 +0530

    like keyword

--- SQL-Queries/like-keyword.sql
+++ SQL-Queries/like-keyword.sql
@@ -1,2 +1,9 @@
 -- like keyword searches a specified pattern which queried, if that pattern matched from any of the data, system reterives the particular data .
 select name from my_details where name like '%iva%';
+
+-- %x — will select all values that begin with x
+-- %x% — will select all values that include x
+-- x% — will select all values that end with x
+-- x%y — will select all values that begin with x and end with y
+-- _x% — will select all values have x as the second character
+-- x_% — will select all values that begin with x and are at least two characters long. You can add additional _ characters to extend the length requirement, i.e. x___%
